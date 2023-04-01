d6cae569226125b1b6aed4a1a66a561f603518fa
Nadin8133 <116784714+Nadin8133@users.noreply.github.com>
2023-04-01 22:59:01 +0300

    Create Python_2HW_1.py

---
+++ Python_2HW_1.py
@@ -0,0 +1,41 @@
+
+a = "Python, welcome!"
+print(a)
+print('Type a = ',type(a))
+b = 2023
+print(b)
+print('Type b = ',type(b))
+c = 3.1415926535
+print(c)
+print('Type c = ',type(c))
+d = bytes(123)
+print(d)
+print('Type d = ',type(d))
+e = ['234', '432', 'gty3', '7uj', [23, 34, 5699]]
+print(e)
+print('Type e = ',type(e))
+f = tuple('hello, world!')
+print(f)
+print('Type f = ',type(f))
+g = set('500')
+print(g)
+print('Type g = ', type(g))
+h = frozenset('599')
+print(h)
+print('Type h = ',type(h))
+j = {'key1': 12,
+    'name_1': 'Nadin',
+     'pet':{
+        'dog': 'Bylka',
+        'сat': 'Masya'
+     }}
+print(j)
+print('Type j = ',type(j))
+x = "Kings"
+y = 'road 33'
+z = x + y
+print(z)
+x1 = 'Kingsroad '
+y1 = 33
+print(x1, 33)
+print(x1 + str(y1))
